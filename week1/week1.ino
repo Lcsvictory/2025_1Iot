9986cd69dc528a0fec839d2f3c41d9925446cfd3
lcsvictory <yuok135z@gmail.com>
2025-03-11 18:18:42 +0900

    morse code SOS

--- week1/week1.ino
+++ week1/week1.ino
@@ -9,17 +9,28 @@ void setup()
 
 void loop()
 {
-  digitalWrite(red, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  for(int i = 0; i < 3; i++) {
+  	digitalWrite(red, HIGH);
+  	delay(200); // Wait for 1000 millisecond(s)
   
-  digitalWrite(red, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(red, LOW);
+  	delay(100); // Wait for 1000 millisecond(s)
+  }
+  delay(200);
+  for(int i = 0; i < 3; i++) {
+  	digitalWrite(green, HIGH);
+  	delay(300); // Wait for 1000 millisecond(s)
   
-  digitalWrite(green, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  	digitalWrite(green, LOW);
+  	delay(200); // Wait for 1000 millisecond(s)
+  }
   
-  digitalWrite(green, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  for(int i = 0; i < 3; i++) {
+  	digitalWrite(red, HIGH);
+  	delay(200); // Wait for 1000 millisecond(s)
   
-  
+  	digitalWrite(red, LOW);
+  	delay(100); // Wait for 1000 millisecond(s)
+  }
+  delay(1000);
 }

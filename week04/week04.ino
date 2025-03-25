a18f5fd0d5f7c35c0b84a8f7091de5ff48c2ff55
Lcsvictory <yuko135z@gmail.com>
2025-03-25 18:18:48 +0900

    lcd 사용예제with 초음파센서

--- week04/week04.ino
+++ week04/week04.ino
@@ -3,20 +3,59 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); //0x27 == lcd주소
 
+int TRIG = 13;
+int ECHO = 12;
+byte smile[8] = { //smile byte code (1인곳만 불이 들어오는 구조.)
+  B00000,
+  B00000,
+  B01010,
+  B00000,
+  B10001,
+  B01110,
+  B00000,
+  B00000
+};
+
 void setup() {
+  // Serial.begin(9600);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
   lcd.init();
   lcd.backlight();
   lcd.print("LCD init");
-  delay(2000);
+  delay(200);
   lcd.clear();
+
+  lcd.createChar(0, smile);
 }
 
 void loop() {
-  lcd.setCursor(0,0);
-  lcd.print("Hello, World!");
   
-  for (int position = 0; position < 16; position++) {
-   	lcd.scrollDisplayLeft();
-    delay(150);
-  }
+
+  long duration, distance;
+  
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  duration = pulseIn(ECHO, HIGH);
+  distance = duration / 58.2;
+  lcd.setCursor(0,0); // 열0~15, 행0~1
+  lcd.print(distance);
+  lcd.printstr(" Cm");
+
+  // lcd.blink(); 커서 깜빡임.
+  // lcd.autoscroll();
+  lcd.setCursor(2,1);
+  lcd.print("by Lcsvictory");
+
+  lcd.setCursor(15, 0);
+  lcd.write(byte(0));
+  delay(500);
+
+  lcd.clear();
+  
 }

45c81b28bbf5e56f4c95a5c253c88a36d053764b
Lcsvictory <yuko135z@gmail.com>
2025-04-15 17:40:51 +0900

    toggle기능을 클래스화 시키기

--- week07/week07.ino
+++ week07/week07.ino
@@ -1,19 +1,11 @@
-int a1 = 2;
-int a2 = 3;
-int a3;
+#include <LedToggle.h>
+
+LedToggle led(LED_BUILTIN);
 
 void setup(){
-  Serial.begin(9600);
-  Serial.println();
-
-  sum(a2,a2,a3);
-  Serial.println(a3);
 }
+
 void loop() {
-
-}
-
-int sum(int a1, int a2, int& a3){
-  a3 = a1+a2;
-  return a3;
+  led.toggle();
+  delay(30);
 }

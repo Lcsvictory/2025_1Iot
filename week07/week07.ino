ce912b23423c07118140b19326328c7ec576488d
Lcsvictory <yuko135z@gmail.com>
2025-04-15 17:06:53 +0900

    SimpleTimer.h 헤더파일 사용

--- week07/week07.ino
+++ week07/week07.ino
@@ -1,21 +1,21 @@
+#include <SimpleTimer.h>
+SimpleTimer timerCnt;
+
+unsigned long loopCnt;
+
+void timerCntFunc() {
+  Serial.println(loopCnt);
+  loopCnt = 0;
+}
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println();
+
+  timerCnt.setInterval(1000, timerCntFunc);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static unsigned long loopCnt = 0;
-  static unsigned long nextMil = millis() + 1000;
-
+  timerCnt.run();
   loopCnt++;
-  // Serial.println(millis());
-
-  if (millis() > nextMil) {
-    nextMil = millis() + 1000;
-    Serial.println(loopCnt);
-    loopCnt = 0;
-    //1초에 대략 280,000번 호출됨. loop함수는.
-  }
 }
